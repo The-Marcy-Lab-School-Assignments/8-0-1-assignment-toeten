da72296d6d810e41f56758bc434eafd52c8ce1af
toeten <n.santiago0228@gmail.com>
2024-09-04 14:24:41 -0400

    why this taking so long

--- server/index.js
+++ server/index.js
@@ -7,28 +7,6 @@ const pathToDistFolder = path.join(__dirname, '../vite-project/dist')
 
 //controllers (including middleware)
 
-//response controllers
-const serveSlash = (req,res,next) => {
-  res.sendFile(__dirname + '/index.html')
-}
-
-const serveAbout = (req, res, next) => {
-  res.send(`
-    <h1>About me</h1>
-    <p>I am so cool like....</p>
-  `);
-};
-
-
-const serveWhat = (req,res,next) => {
-  const {name, favFood} = req.query
-  res.send(`My name is ${name} and my favorite food is ${favFood}`)
-}
-
-const serveData = (req,res,next) => {
-  const coolPeople = [{name: 'nicole'}, {name: 'javier'}, {name: 'noe'}]
-  res.send(coolPeople)
-}
 
 
 //middleware
@@ -44,12 +22,6 @@ const serveData = (req,res,next) => {
 //app.use (middleware)
 app.use(logRoutes)
 app.use(serveStatic)
-
-//app.get (response controllers)
-app.get('/', serveSlash)
-app.get('/about', serveAbout)
-app.get('/api/what', serveWhat)
-app.get('/api/data', serveData)
 
 const port = '8080'
 
